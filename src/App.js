1aca99e598bca9d6442fbbee2f809c66a6b209b1
Johneriksson88 <john.e.eriksson@gmail.com>
2023-02-16 22:25:59 +0000

    Add post page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import PostCreateForm from './pages/posts/PostCreateForm';
+import PostPage from "./pages/posts/PostPage";
 
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+              <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
 
